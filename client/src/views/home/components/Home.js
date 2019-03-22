eb30d1d58fadaad407a9a83818791f0b37d11135
Mone't Fulgham <monetfulgham@gmail.com>
2019-03-22 15:05:03 -0400

    Add grid to <Home/> for responsiveness

--- client/src/views/home/components/Home.js
+++ client/src/views/home/components/Home.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Header from './Header';
 import SearchBar from './SearchBar'
 import JobBoard from './JobBoard'
+import Grid from '@material-ui/core/Grid';
+
 
 const styles = {
     parent: {
@@ -25,16 +27,23 @@ export default class Home extends Component {
 
     render() {
         return (
-            <div className="parentContainer" style={styles.parent}>
-                <div className="body">
-                    <Header/>
-                </div>
-                <div className="search">
-                    <SearchBar searchTerm={this.handleSearch}/>
-                </div>
-                <div className="job-board">
-                    <JobBoard search={this.state.searchTerm}/>
-                </div>
+            <div>
+                <Grid container spacing={24}>
+                    <Grid item>
+                        <Header/>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={24} direction="column"
+                      alignItems="center">
+                    <Grid item xs={12} justify="center"
+                          alignItems="space-around">
+                        <SearchBar searchTerm={this.handleSearch}/>
+                    </Grid>
+                    <Grid item xs={10}
+                          alignItems="center">
+                        <JobBoard search={this.state.searchTerm}/>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
