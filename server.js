47e4c26663cde8d54b20f3cf758550f56277fdbd
Mone't Fulgham <monetfulgham@gmail.com>
2019-03-23 18:52:16 -0400

    update scripts and server

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname = 'client/build/index.html'));
     })
 }
 
