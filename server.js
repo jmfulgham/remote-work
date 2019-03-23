9aeb5f931ecee9e8e02206df22c57387c22816f5
Mone't Fulgham <monetfulgham@gmail.com>
2019-03-23 17:30:12 -0400

    trying to reconfigure server for deploy

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const axios = require('axios');
-
+const path = require('path');
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    })
 }
+
+
 
 app.get('/api/github', (req, res) => {
     console.log(`first call made from ${port}`);
