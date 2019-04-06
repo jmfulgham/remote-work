b51076dc7d5e965e7ab11cda4196aad5eb5a698f
Mone't Fulgham <monetfulgham@gmail.com>
2019-04-06 12:31:19 -0400

    Reformat server.js and refactor WWR service

--- client/src/services/WeWorkRemotelyService.js
+++ client/src/services/WeWorkRemotelyService.js
@@ -3,30 +3,39 @@ let parser = new Parser();
 
 export default class WeWorkRemotelyService {
 
-    async getWeWorkRemotelyDevOpsJobs() {
+    async getWWRDevOpsJobs() {
         let soRssUrl = '/api/weWorkRemotely/devOps';
         let feed = await parser.parseURL(soRssUrl).catch(e => {
-            return `S.O. error, ${e}`
+            return `WWR DevOps. error, ${e}`
         });
-        return this.handleRSSFeed(feed.items);
+        return feed.items;
     }
 
     async getWWRProductJobs(){
         let soRssUrl = '/api/weWorkRemotely/product';
         let feed = await parser.parseURL(soRssUrl).catch(e => {
-            return `S.O. error, ${e}`
+            return `WWR Product error, ${e}`
         });
-        return this.handleRSSFeed(feed.items);
+        return feed.items;
     }
 
     async getWWRDesignJobs(){
         let soRssUrl = '/api/weWorkRemotely/design';
         let feed = await parser.parseURL(soRssUrl).catch(e => {
-            return `S.O. error, ${e}`
+            return `WWR Design error, ${e}`
         });
-        return this.handleRSSFeed(feed.items);
+        return feed.items;
     }
 
+   async concatAndFormatFeed(feed){
+        let allWwrJobs;
+        let designJobs = await this.getWWRDesignJobs();
+       let productJobs= await this.getWWRProductJobs();
+       let devOpsJobs = await this.getWWRDevOpsJobs();
+       allWwrJobs = designJobs.concat(productJobs,devOpsJobs);
+       return this.handleRSSFeed(allWwrJobs);
+
+    }
 
     handleRSSFeed(feed) {
         return feed.map(job => {
