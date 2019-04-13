57c1896e9fcbf8c481cd1ba07af9b1edd6ca146e
Mone't Fulgham <monetfulgham@gmail.com>
2019-04-13 16:17:18 -0400

    Update links

--- client/src/views/home/components/Header.js
+++ client/src/views/home/components/Header.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import { NavLink } from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import MaterialIcon from 'material-icons-react';
 
 require("typeface-quicksand");
 
@@ -11,31 +10,39 @@ const styles = {
     social: {
         display: 'flex',
         marginLeft: "auto",
-        alignItems: 'center'
+        alignItems: 'center',
+
     }
 };
 
 export default class Header extends Component {
     render() {
         return (<div className={"app-bar"}>
-                    <AppBar elevation={1} >
-                        <Toolbar>
-                            <Typography variant="display1" color="secondary"><NavLink
-                                to="/"
-                                activeStyle={{
-                                    color: "#A24FFC",
-                                    textDecoration: 'none'
-                                }}
-                            >
-                                remotework(tech)
-                            </NavLink></Typography>
-                            <Typography variant="subtitle1" style={{color:"#27C4A8"}}>beta</Typography>
-                            <div style={styles.social}>
-                                <a href="mailto:mo@jaimo.net?subject=Hey! I love remoteWork.tech!"><MaterialIcon icon="email" color='#27C4A8' aria-label="Email"/></a>
-                            </div>
-                            <NavLink to="/post-job">Post a Job!</NavLink>
-                        </Toolbar>
-                    </AppBar>
+                <AppBar elevation={1}>
+                    <Toolbar>
+                        <Typography variant="display1" color="secondary"><NavLink
+                            to="/"
+                            activeStyle={{
+                                color: "#A24FFC",
+                                textDecoration: 'none'
+                            }}>remotework(tech)</NavLink></Typography>
+
+                        <Typography variant="subtitle1" style={{color: "#27C4A8"}}>beta</Typography>
+
+                        <div style={styles.social}>
+                            <Typography>  <a href="mailto:mo@jaimo.net?subject=Hey! I love remoteWork.tech!"
+                               style={{textDecoration: 'none', color: "#27C4A8"}}>Contact
+                            </a></Typography>
+
+                            <Typography><NavLink to="/post-job"
+                                 style={{
+                                     color: "#27C4A8",
+                                     textDecoration: 'none',
+                                     margin: '1rem'
+                                 }}> Post a Job!</NavLink></Typography>
+                        </div>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
