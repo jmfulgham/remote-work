a3150bdaafac9df1c758511344832547c12f0f51
Mone't Fulgham <monetfulgham@gmail.com>
2019-04-30 15:46:59 -0400

    Add Contact link with react router and dependencies etc and add logo

--- client/src/views/home/components/Header.js
+++ client/src/views/home/components/Header.js
@@ -1,31 +1,46 @@
 import React, {Component} from 'react';
+import {NavLink} from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import MaterialIcon from 'material-icons-react';
 
+const logo = require("../../../assets/rwt_logo4.png");
 require("typeface-quicksand");
 
 const styles = {
+
     social: {
         display: 'flex',
         marginLeft: "auto",
-        alignItems: 'center'
+        alignItems: 'center',
+
     }
 };
 
 export default class Header extends Component {
     render() {
-        return (<div className={"app-bar"}>
-                    <AppBar elevation={1} >
-                        <Toolbar>
-                            <Typography variant="display1" color="secondary">remoteWork(tech)</Typography>
-                            <Typography variant="subtitle1" style={{color:"#27C4A8"}}>beta</Typography>
-                            <div style={styles.social}>
-                                <a href="mailto:mo@jaimo.net?subject=Hey! I love remoteWork.tech!"><MaterialIcon icon="email" color='#27C4A8' aria-label="Email"/></a>
-                            </div>
-                        </Toolbar>
-                    </AppBar>
+        return (<div className={"app-bar"} style={{marginBottom: '1rem'}}>
+                <AppBar elevation={0} color={"#EBEBEB"}>
+                    <Toolbar>
+                        <NavLink
+                            to="/"
+                            activeStyle={{
+                                color: "#A24FFC",
+                                textDecoration: 'none'
+                            }}>
+                            <img src={logo} alt="remotework(tech)"/>
+                        </NavLink>
+                        <Typography variant="subtitle1" style={{color: "#27C4A8"}}>beta</Typography>
+                        <div style={styles.social}>
+                            <Typography variant="h6">
+                                <a href="mailto:mo@jaimo.net?subject=Hey! I love remoteWork.tech!"
+                                   style={{textDecoration: 'none', color: "#27C4A8"}}>
+                                    Contact
+                                </a>
+                            </Typography>
+                        </div>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
