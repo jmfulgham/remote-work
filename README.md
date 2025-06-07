434ec84cfc77e3ad33b7851061df7ed72e5440ef
Mone't Fulgham <monetfulgham@gmail.com>
2025-06-07 18:34:12 -0400

    Refactor application for modern stack

--- README.md
+++
@@ -1,3 +0,0 @@
-## remote-work.tech
-
-Beta of remotework.tech, an aggregator of tech remote jobs across the web's most popular remote job sites.
--- server.js
+++ server.js
@@ -1,98 +1,53 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 8080;
-const axios = require('axios');
-const path = require('path');
-const helmet = require('helmet');
-const uuidv4 = require('uuid/v4');
 
-const favicon = require('serve-favicon');
-const nonce = new Buffer.from(uuidv4()).toString('base64');
-
-app.use(helmet());
 app.use((req, res, next) => {
-    res.locals.styleNonce = Buffer.from(uuidv4()).toString('base64');
     next();
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'",],
-            fontSrc: ["'self'", 'https://fonts.google.com/'],
-            styleSrc: ["'self'", (req, res) => `'nonce-${res.locals.styleNonce}'`],
-            sandbox: ['allow-forms', 'allow-scripts'],
-            reportUri: '/report-violation',
-            objectSrc: ["'self'"],
-            upgradeInsecureRequests: true,
-            workerSrc: false
-        },
-        loose: false,
-        reportOnly: false,
-        setAllHeaders: false,
-        disableAndroid: false,
-        browserSniff: true
-    });
 });
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.use(favicon(path.join(__dirname,'client', 'public', 'rwtfavicon.ico')));
-
-app.get('/', (req, res) => {
-    res.render('index', {styleNonce: res.locals.styleNonce})
-});
-
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));
-    })
-}
-
-app.get('/api/github', (req, res) => {
-    console.log(`first call made from ${port}`);
-    let url = 'https://jobs.github.com/positions.json?&location=remote&page=1';
-    axios({
-        method: 'get',
-        url
-    })
-        .then(resp => res.send(resp.data)).catch(e => `Server error, ${e}`);
-});
-
-app.get('/api/remoteOkRss', (req, res) => {
+//TODO error handling
+app.get('/api/remoteOkRss', async (req, res) => {
     let url = 'https://remoteok.com/rss';
-    const resp = axios({
-        method: 'get',
-        url
-    })
-    console.log({resp})
-        // .then(resp => res.send(resp.data)).catch(e => `Server error, ${e}`);
+    try {
+        const feed = await fetch(url)
+        res.send(await feed.text())
+    } catch (e){
+        console.log({e})
+        res.send(e)
+    }
 });
 
-app.get('/api/weWorkRemotely/devOps', (req, res) => {
+app.get('/api/weWorkRemotely/devOps', async (req, res) => {
     let url = 'https://weworkremotely.com/categories/remote-devops-sysadmin-jobs.rss';
-    axios({
-        method: 'get',
-        url,
-    }).then(resp => res.send(resp.data)).catch(e => `Server error, ${e}`);
+    try {
+        const feed = await fetch(url)
+        res.send(await feed.text())
+    } catch (e){
+        console.log({e})
+        res.send(e)
+    }
 });
 
-app.get('/api/weWorkRemotely/product', (req, res) => {
+app.get('/api/weWorkRemotely/product', async (req, res) => {
     let url = 'https://weworkremotely.com/categories/product.rss';
-    axios({
-        method: 'get',
-        url,
-    }).then(resp => res.send(resp.data)).catch(e => `Server error, ${e}`);
+    try {
+        const feed = await fetch(url)
+        res.send(await feed.text())
+    } catch (e){
+        console.log({e})
+        res.send(e)
+    }
 });
 
-app.get('/api/weWorkRemotely/design', (req, res) => {
+app.get('/api/weWorkRemotely/design',  async (req, res) => {
     let url = 'https://weworkremotely.com/categories/remote-design-jobs.rss';
-    axios({
-        method: 'get',
-        url,
-    }).then(resp => res.send(resp.data)).catch(e => `Server error, ${e}`);
+    try {
+        const feed = await fetch(url)
+        res.send(await feed.text())
+    } catch (e){
+        console.log({e})
+        res.send(e)
+    }
 });
